56ee39ed304c9350d5b787e8078ddf450e60132f
Kluy <kluy@i.ua>
2023-12-20 10:54:53 +0200

    new api fix

--- src/gateway/gateway.js
+++ src/gateway/gateway.js
@@ -1,7 +1,11 @@
-const baseUrl = 'https://api.iev.aero/api/flights/';
+// const baseUrl = 'https://api.iev.aero/api/flights/';
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/airport/';
 
-export const getFlights = (date) => {
+export const getFlights = date => {
   return fetch(`${baseUrl}${date}`)
-    .then((response) => response.json())
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
     .catch(() => console.log('Internal Server Error. Can"t display flights'));
 };
